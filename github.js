9a24b2e78d8379692c4448ebc0256538134951a2
Blake Pond <blakehp@icloud.com>
2021-12-07 16:16:44 -0700

    updated git and github commands and notes

--- github.js
+++ github.js
@@ -12,9 +12,38 @@ git commands
 - git commit - commit a file to repo
 - git status - 
 - git log - check history 
+- git branch [branch-name]
+    - git branch -r 
+        get all branches on local machine
+- git checkout [branch-name]
+- git checkout -b "name of new branch"
+- git push -u [origin][branch]
+- git pull 
+- git fetch 
+    get all the branches from remote 
+- git tag [tag name] [branch]
+    -a = create annotated tag
+    git push --tags
+        push tags up to github
 
 ..github..
+branching strategies
+- centralized workflow
+- gitflow workflow 
+- forking workflow
+    -make changes to copy with out affecting origninal repo
 
+- github workflow 
+    - start with the main branch
+    - create new branch from main
+    - pull request
+    - reviews 
+    - merge to main branch
+        merging with conflicts
+            editing on the same line 
+            editing a deleted file
+
+            
 
 
 
