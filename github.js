c626aa325800aaf7af789708c813765793355d47
Blake Pond <blakehp@icloud.com>
2021-12-07 10:27:08 -0700

    git notes

---
+++ github.js
@@ -0,0 +1,23 @@
+/* 
+..git..
+- working directory
+- staging area
+-.git repo
+
+git commands
+- git config
+- git init - initialize git repo
+- git clone - download project from remote
+- git add - prepare file for staging
+- git commit - commit a file to repo
+- git status - 
+- git log - check history 
+
+..github..
+
+
+
+
+
+
+*/
