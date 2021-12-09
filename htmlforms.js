5f8cad5945790da339181948c6b624c0bd96a04d
Blake Pond <blakehp@icloud.com>
2021-12-09 09:25:16 -0700

    html notes added

---
+++ htmlforms.js
@@ -0,0 +1,35 @@
+/*
+HTML
+line-height
+    - unfixed line style, changes space between text
+    - adjusts automatically
+text-transform
+    - set the elements as capitalized or uppercased
+font-family
+    - web-safe fonts
+    - cssfontstack.com
+    - font stack
+        - set a default font if the users browser doesn't have the font
+    - externally hosted fonts
+        - google fonts
+            - link the url in the head of html
+    - letter spacing: adjusts space between text
+    - relative units
+        -%
+        -em - relative to the parents font size
+        -rem - relative to the font size root element like body or html
+    - border
+        - width, style, color
+        - border-left,right,top,bottom
+    - outline
+        -width, style, color
+    - padding
+        - adding space inside the element
+        - top right bottom left
+        - top/bottom left/right
+    - margin
+        - adding space outside the element
+
+
+
+*/
