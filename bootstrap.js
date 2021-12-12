3d18ddf31c92854ef1c53ac29e66415cf1ca19f7
Blake Pond <blakehp@icloud.com>
2021-12-11 20:16:59 -0700

    bootstrap notes added

---
+++ bootstrap.js
@@ -0,0 +1,30 @@
+/*
+bootstrap- open source css framework
+    - layout
+    - components
+    - interactivity
+    - style utilities
+    - themeing support
+
+    adding bootstrap to a site
+        - CDN (content delivery network)
+        - Package manager NPM Yarn etc
+        - download css files (reference local file on server)
+        - compile
+    
+        starting with the mobile sizes and working up
+            - <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        classes
+            - "btn btn-success" 
+            - "btn btn-info"
+            - "m-" margin 
+            - "p-" padding
+            - "fw-" font width
+            - "fst- " font style
+
+            alert class
+                - "alert alert-success mt-"
+
+
+*/
